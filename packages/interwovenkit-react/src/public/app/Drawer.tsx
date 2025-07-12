7e63919e9ffb1277436061321c48ea42f938dfb6
alecande11 <54709706+alecande11@users.noreply.github.com>
2025-07-12 19:42:21 +0200

    Fix drawer scroll

--- packages/interwovenkit-react/src/public/app/Drawer.tsx
+++ packages/interwovenkit-react/src/public/app/Drawer.tsx
@@ -89,8 +89,8 @@ const Drawer = ({ children }: PropsWithChildren) => {
 
       {drawerTransition((style, item) =>
         item ? (
-          <animated.div style={style} className={clsx(styles.content, "body")}>
-            <div ref={setContainer}>
+          <animated.div style={style} className={clsx(styles.drawer)}>
+            <div className={clsx(styles.content, "body")} ref={setContainer}>
               <TxWatcher />
               <WidgetHeader />
               <AsyncBoundary errorBoundaryProps={errorBoundaryProps}>{children}</AsyncBoundary>
