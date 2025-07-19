0618f1cbd1bce8bca9320a265e07b7e54d4c1d14
sim <sim@cheolhwan.com>
2025-07-19 23:50:03 +0900

    feat(activity): update ActivityChanges component

--- packages/interwovenkit-react/src/pages/wallet/tabs/activity/ActivityChanges.tsx
+++ packages/interwovenkit-react/src/pages/wallet/tabs/activity/ActivityChanges.tsx
@@ -3,13 +3,15 @@ import BigNumber from "bignumber.js"
 import { computeAddress } from "ethers"
 import { fromBase64, toHex } from "@cosmjs/encoding"
 import { formatAmount, truncate } from "@/public/utils"
-import { useHexAddress } from "@/public/data/hooks"
+import { useHexAddress, useInitiaAddress } from "@/public/data/hooks"
 import { denomToMetadata, useFindAsset } from "@/data/assets"
 import type { NormalizedChain } from "@/data/chains"
 import AsyncBoundary from "@/components/AsyncBoundary"
+import type { BaseAsset } from "@/components/form/types"
 import WithMoveResource from "../assets/WithMoveResource"
-import type { TxItem } from "./data"
+import { getCoinChanges } from "./changes/changes"
 import { calcChangesFromEvents } from "./calc"
+import type { TxItem } from "./data"
 import WithDenom from "./WithDenom"
 import styles from "./ActivityChanges.module.css"
 
@@ -17,7 +19,20 @@ interface Props extends TxItem {
   chain: NormalizedChain
 }
 
-const ActivityChanges = ({ tx, events, chain }: Props) => {
+const ActivityChange = ({ amount, asset }: { amount: string; asset: BaseAsset }) => {
+  const { denom, symbol, decimals } = asset
+  const isPositive = new BigNumber(amount).isPositive()
+  const absAmount = new BigNumber(amount).abs().toString()
+
+  return (
+    <div className={clsx(styles.change, isPositive ? styles.positive : styles.negative)}>
+      {isPositive ? "+" : "-"}
+      {formatAmount(absAmount, { decimals })} {symbol ?? truncate(denom)}
+    </div>
+  )
+}
+
+const ActivityChangesWithMetadata = ({ tx, events, chain }: Props) => {
   const hexAddress = useHexAddress()
   const findAsset = useFindAsset(chain)
 
@@ -29,36 +44,47 @@ const ActivityChanges = ({ tx, events, chain }: Props) => {
   const fee = tx.auth_info.fee?.amount[0] ?? { amount: "0", denom: "" }
   const isPaidByMe = signerHexAddress === hexAddress && !payer
 
-  if (!(chain.metadata?.is_l1 || chain.metadata?.minitia?.type === "minimove")) {
-    return null
-  }
-
   const feeMetadata = denomToMetadata(fee.denom)
   const feeWithMetadata = { amount: isPaidByMe ? fee.amount : "0", metadata: feeMetadata }
   const changes = calcChangesFromEvents(events, feeWithMetadata, hexAddress)
 
   return changes.map(({ amount, metadata }, index) => {
-    const isPositive = new BigNumber(amount).isPositive()
-    const absAmount = new BigNumber(amount).abs().toString()
     return (
       <AsyncBoundary suspenseFallback={null} errorBoundaryProps={{ fallback: null }} key={index}>
         <WithDenom metadata={metadata} chain={chain}>
           {(denom) => (
             <WithMoveResource asset={findAsset(denom)} chain={chain}>
-              {({ denom, symbol, decimals }) => (
-                <div
-                  className={clsx(styles.change, isPositive ? styles.positive : styles.negative)}
-                >
-                  {isPositive ? "+" : "-"}
-                  {formatAmount(absAmount, { decimals })} {symbol ?? truncate(denom)}
-                </div>
-              )}
+              {(asset) => <ActivityChange amount={amount} asset={asset} />}
             </WithMoveResource>
           )}
         </WithDenom>
       </AsyncBoundary>
     )
   })
+}
+
+const ActivityChangesWithDenom = ({ events, chain }: Props) => {
+  const address = useInitiaAddress()
+  const changes = getCoinChanges(events, address)
+  const findAsset = useFindAsset(chain)
+
+  return changes.map(({ amount, denom }, index) => {
+    return (
+      <div key={index}>
+        <ActivityChange amount={amount} asset={findAsset(denom)} />
+      </div>
+    )
+  })
+}
+
+const ActivityChanges = (props: Props) => {
+  const { chain } = props
+
+  if (chain.metadata?.is_l1 || chain.metadata?.minitia?.type === "minimove") {
+    return <ActivityChangesWithMetadata {...props} />
+  }
+
+  return <ActivityChangesWithDenom {...props} />
 }
 
 export default ActivityChanges
