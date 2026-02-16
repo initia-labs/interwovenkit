3c5873d6100fd620ab12a6df153ee3b4f8fbeaf8
Sim Cheolhwan <sim@23labs.io>
2026-02-16 13:52:47 +0900

    refactor(wallet): use build-time version constant
    - Replace direct package.json import with
      __INTERWOVENKIT_VERSION__ in Version.tsx

--- packages/interwovenkit-react/src/pages/wallet/components/Version.tsx
+++ packages/interwovenkit-react/src/pages/wallet/components/Version.tsx
@@ -1,8 +1,7 @@
-import { version } from "@/../package.json"
 import styles from "./Version.module.css"
 
 const Version = () => {
-  return <aside className={styles.version}>v{version}</aside>
+  return <aside className={styles.version}>v{__INTERWOVENKIT_VERSION__}</aside>
 }
 
 export default Version
