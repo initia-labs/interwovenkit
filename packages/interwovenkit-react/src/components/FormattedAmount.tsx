a8c9075fb346c14fe6dc0debaa3c958b558d03c4
tansawit <sawit.tr@gmail.com>
2026-02-18 14:19:01 +0700

    fix: use unicode-style tiny balance subscript

--- packages/interwovenkit-react/src/components/FormattedAmount.tsx
+++ packages/interwovenkit-react/src/components/FormattedAmount.tsx
@@ -1,25 +1,15 @@
-import { formatDisplayAmountParts } from "@/lib/format"
-import styles from "./FormattedAmount.module.css"
+import { formatDisplayAmount } from "@/lib/format"
 
 import type { ComponentPropsWithoutRef } from "react"
 
 interface Props extends ComponentPropsWithoutRef<"span"> {
-  amount: Parameters<typeof formatDisplayAmountParts>[0]
+  amount: Parameters<typeof formatDisplayAmount>[0]
   decimals: number
   dp?: number
 }
 
 const FormattedAmount = ({ amount, decimals, dp, ...props }: Props) => {
-  const { formatted, subscript } = formatDisplayAmountParts(amount, { decimals, dp })
-  if (!subscript) return <span {...props}>{formatted}</span>
-
-  return (
-    <span {...props}>
-      {subscript.prefix}
-      <sub className={styles.sub}>{subscript.hiddenZeroCount}</sub>
-      {subscript.significant}
-    </span>
-  )
+  return <span {...props}>{formatDisplayAmount(amount, { decimals, dp })}</span>
 }
 
 export default FormattedAmount
